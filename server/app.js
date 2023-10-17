05ddbfbae76d3bc23283de4f69e91e9088398c38
Abdulaziz <103261897+abdulazizbay@users.noreply.github.com>
2023-10-17 20:27:41 +0900

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const authMiddleware = require("./middlewares/authMiddleware")
 
 app.use(cors(
     {
-        origin: ["https://deploy-mern-1whq.vercel.app"],
+        origin: ["https://mern-ecommerce-nu3p.vercel.app/"],
         method: ["GET", "POST"],
         credentials:true
     }
