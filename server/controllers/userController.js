1c0d6018801c650b6fc3af86d1964471c2fed85d
abdulazizbay7 <abdulazizxalilov30@gmail.com>
2024-02-08 03:02:15 +0900

    vercel

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,117 +6,56 @@ const jwt = require('jsonwebtoken')
 const config = require('config')
 
 
-// exports.register = (req, res)=>{
-//     try{
-//         const {name,email,password} = req.body;
-//         const userExist =  User.findOne({email})
-//         if(userExist){
-//             return res.status(400).send({message:"User already exists"})
-//         }
-//         // const hashedPassword =  bcrypt.hash(password,12)
-//         const user = new User({email:email,name:name,password:"hashedPassword"})
-//          user.save()
-//         res.status(200).send({message:"User created successfully"})
-//     }catch(err){
-//         res.status(400).send({message: err})
-//         console.log(err, ':error');
-//     }
-// }
+exports.register = async(req, res)=>{
+    try{
+        const {name,email,password} = req.body;
+        const userExist = await User.findOne({email})
+        if(userExist){
+            return res.status(400).send({message:"User already exists"})
+        }
+        const hashedPassword = await bcrypt.hash(password,12)
+        const user = new User({email:email,name:name,password:hashedPassword})
+        await user.save()
+        res.status(200).send({message:"User created successfully"})
+    }catch(err){
+        res.status(400).send({message: err})
+        console.log(err, ':error');
+    }
+}
 
-// exports.login =  (req, res) => {
-//     try {
-//         const { name, email, password } = req.body;
-//         const userExist =  User.findOne({ email });
-//         if (!userExist) {
-//             return res.status(400).json({ message: 'User not found, please create an account first.' });
-//         }
-//         // const checkedUser =  bcrypt.compare(password, userExist.password);
-//         if (!"checkedUser") {
-//             return res.status(404).json({ message: 'Password mismatch' });
-//         }
-//         // const token = jwt.sign(
-//         //     { userId: userExist.id },
-//         //     config.get('jwtSecret'),
-//         //     { expiresIn: '1d' }
-//         // );
-//         // res.cookie("accessToken", token, {
-//         //     httpOnly: true
-//         // });
-//         res.status(200).json({ token, userID: userExist.id });
-//     } catch (err) {
-//         console.error(err);
-//         res.status(400).json({ message: 'An error occurred during login.' });
-//     }
-// };
+exports.login = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const userExist = await User.findOne({ email });
+        if (!userExist) {
+            return res.status(400).json({ message: 'User not found, please create an account first.' });
+        }
+        const checkedUser = await bcrypt.compare(password, userExist.password);
+        if (!checkedUser) {
+            return res.status(404).json({ message: 'Password mismatch' });
+        }
+        const token = jwt.sign(
+            { userId: userExist.id },
+            config.get('jwtSecret'),
+            { expiresIn: '1d' }
+        );
+        res.cookie("accessToken", token, {
+            httpOnly: true
+        });
+        res.status(200).json({ token, userID: userExist.id });
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({ message: 'An error occurred during login.' });
+    }
+};
 
-exports.profileGet =  (req, res) => {
+exports.profileGet = async (req, res) => {
     try {
         const userId = req.user.userId
-        const user =  User.findById(userId)
+        const user = await User.findById(userId)
         res.status(200).json({name:user.name, email:user.email})
     }catch (err) {
         console.log(err.message)
         res.status(500).json({ message:"went wrong getting profile"})
     }
 }
-// const express   = require('express');
-// const router = express.Router();
-// const bcrypt = require('bcrypt');
-// const User = require('../models/User')
-// const jwt = require('jsonwebtoken')
-// const config = require('config')
-//
-//
-// exports.register = async(req, res)=>{
-//     try{
-//         const {name,email,password} = req.body;
-//         const userExist = await User.findOne({email})
-//         if(userExist){
-//             return res.status(400).send({message:"User already exists"})
-//         }
-//         const hashedPassword = await bcrypt.hash(password,12)
-//         const user = new User({email:email,name:name,password:hashedPassword})
-//         await user.save()
-//         res.status(200).send({message:"User created successfully"})
-//     }catch(err){
-//         res.status(400).send({message: err})
-//         console.log(err, ':error');
-//     }
-// }
-//
-// exports.login = async (req, res) => {
-//     try {
-//         const { name, email, password } = req.body;
-//         const userExist = await User.findOne({ email });
-//         if (!userExist) {
-//             return res.status(400).json({ message: 'User not found, please create an account first.' });
-//         }
-//         const checkedUser = await bcrypt.compare(password, userExist.password);
-//         if (!checkedUser) {
-//             return res.status(404).json({ message: 'Password mismatch' });
-//         }
-//         const token = jwt.sign(
-//             { userId: userExist.id },
-//             config.get('jwtSecret'),
-//             { expiresIn: '1d' }
-//         );
-//         res.cookie("accessToken", token, {
-//             httpOnly: true
-//         });
-//         res.status(200).json({ token, userID: userExist.id });
-//     } catch (err) {
-//         console.error(err);
-//         res.status(400).json({ message: 'An error occurred during login.' });
-//     }
-// };
-//
-// exports.profileGet = async (req, res) => {
-//     try {
-//         const userId = req.user.userId
-//         const user = await User.findById(userId)
-//         res.status(200).json({name:user.name, email:user.email})
-//     }catch (err) {
-//         console.log(err.message)
-//         res.status(500).json({ message:"went wrong getting profile"})
-//     }
-// }
--- server/index.js
+++ server/index.js
@@ -1,42 +1,3 @@
-// const express = require('express')
-// const mongoose = require('mongoose')
-// const cors = require('cors')
-// const RegisterModel = require('./models/Cart')
-//
-// const app = express()
-// app.use(cors(
-//     {
-//         origin: ["https://deploy-mern-frontend.vercel.app"],
-//         methods: ["POST", "GET"],
-//         credentials: true
-//     }
-// ));
-// app.use(express.json())
-//
-// mongoose.connect('mongodb+srv://abdulazizxalilov30:mohlaroyim@cluster0.ltxifzp.mongodb.net/?retryWrites=true&w=majority');
-//
-//
-// app.get("/", (req, res) => {
-//     res.json("Hello");
-// })
-// app.post('/register', (req, res) => {
-//     const {name, email, password} = req.body;
-//     RegisterModel.findOne({email: email})
-//         .then(user => {
-//             if(user) {
-//                 res.json("Already have an account")
-//             } else {
-//                 RegisterModel.create({name: name, email: email, password: password})
-//                     .then(result => res.json(result))
-//                     .catch(err => res.json(err))
-//             }
-//         }).catch(err => res.json(err))
-// })
-//
-//
-// app.listen(3001, () => {
-//     console.log("Server is Running")
-// })
 const express = require('express')
 const app = express();
 const session = require('express-session');
@@ -47,17 +8,17 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const authMiddleware = require("./middlewares/authMiddleware")
 
-app.use(cors(
-    {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
-        method: ["GET", "POST"],
-        credentials:true
-    }
-))
+// app.use(cors(
+//     {
+//         origin: ["https://mern-ecommerce-nu3p.vercel.app"],
+//         method: ["GET", "POST"],
+//         credentials:true
+//     }
+// ))
 
-// const errorHandler = (err, req, res, next) => {
-//     res.status(500).json({ message: 'Internal server error' });
-// };
+const errorHandler = (err, req, res, next) => {
+    res.status(500).json({ message: 'Internal server error' });
+};
 // app.use(authMiddleware);
 app.use(express.json({extended: true}));
 app.use(cors())
@@ -68,39 +29,31 @@ app.use('/api/product',require('./routes/productRouter'))
 app.use('/api/category',require('./routes/categoryRouter'))
 app.use('/api/cart',require('./routes/cartRouter'))
 app.use('/api/search',require('./routes/searchProduct'))
-// app.use(errorHandler);
-
-app.get("/", (req, res) => {
-    res.json("Hello1");
-})
+app.use(errorHandler);
 
 
 
-// app.get('api/protected-route', authMiddleware,(req, res) => {
-//     res.send('Welcome to the protected route!');
-// });
 
-// async function start() {
-//     try {
-//         await mongoose.connect("mongodb+srv://abdulazizxalilov30:mohlaroyim@cluster0.ltxifzp.mongodb.net/?retryWrites=true&w=majority", {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true
-//         })
-//
-//
-//         app.listen(3001, () => {
-//             console.log(`Successfully running`);
-//         });
-//     } catch (e) {
-//         console.error(e);
-//         process.exit(1);
-//     }
-// }
-//
-// start();
-mongoose.connect("mongodb+srv://abdulazizxalilov30:mohlaroyim@cluster0.ltxifzp.mongodb.net/?retryWrites=true&w=majority")
+app.get('api/protected-route', authMiddleware,(req, res) => {
+    res.send('Welcome to the protected route!');
+});
 
-app.listen(3001, () => {
-    console.log("Server is Running")
-})
+async function start() {
+    try {
+        await mongoose.connect(config.get("mongoURI"), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+
+        app.listen(config.get("port"), () => {
+            console.log(`Successfully running`);
+        });
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+}
+
+start();
 
